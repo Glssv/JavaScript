8a828e12afcd82cafc04f2fe58762e6e137c5bbd
Gracielle da silva <graciellessv@gmail.com>
2023-01-26 16:21:51 -0300

    Update teste2.js

--- exercicios/teste2.js
+++ exercicios/teste2.js
@@ -1,6 +1,6 @@
 let pais= 'França'
 console.log(`vivendo em ${pais}`)
-if (pais == Brasil){
+if (pais == 'Brasil'){
     console.log(`você é brasileiro`)
 } else {
     console.log(`você é estrangeiro`)
