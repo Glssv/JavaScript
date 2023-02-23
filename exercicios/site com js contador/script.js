ba6765d476597fda2c121b54f2b60e568ff9e653
Gracielle da silva <graciellessv@gmail.com>
2023-02-23 13:04:14 -0300

    vamos contar

--- exercicios/site com js contador/script.js
+++ exercicios/site com js contador/script.js
@@ -1,17 +1,20 @@
-function contar(){
+function contar() {
     let ini= document.getElementById('txti')
     let fim= document.getElementById('txtf')
     let passo= document.getElementById('txtp')
     let res= document.getElementById('res')
 
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] faltam dados!')
     } else{
-        res.innerHTML= 'contando...'
-        let i= Number(ini.value)
+        res.innerHTML= 'contando: '
+        let i= Number(ini.value) // converte o ini pra numero
         let f= Number(fim.value)
         let p= Number(passo.value)
 
-        for(let c= 1; c <= f )
+        for(let c= i; c <= f; c += p) {
+        res.innerHTML += ` ${c} \u{1F449}` 
+        } 
+        res.innerHTML += `\u{26A0}`
     }
 }
